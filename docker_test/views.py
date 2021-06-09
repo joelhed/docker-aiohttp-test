a48e5e96e915aae3f9e690196d7479dd7e34c9a6
Joel Hedtjärn <joel.hedtjarn@gmail.com>
2021-06-09 16:26:13 +0200

    Finish the API
     * Implement PUT /polls/{poll_id}/choices/{choice_id}
     * Update GET / for some status information about the web service
     * Add view documentation

--- docker_test/views.py
+++ docker_test/views.py
@@ -6,31 +6,38 @@ from .db import create_connection
 
 
 async def index(request):
+    """Get the status of the application."""
     async with create_connection(request.app) as conn:
-        cur = await conn.execute("""
-        SELECT question.id, question.pub_date, question.question_text, choice.choice_text, choice.votes
-        FROM choice
-        LEFT JOIN question on choice.question_id=question.id;""")
-        data = await cur.fetchall()
-        await cur.close()
-        #conf = request.app["config"]
-        return web.json_response(data)
+        cur = await conn.execute("SELECT COUNT(*) FROM question;")
+        question_count, = await cur.fetchone()
+    return web.json_response({
+        "status": "online",
+        "question_count": question_count,
+    })
 
 
-# Resources
+# POLLS
 
-# Poll
-# /polls
-# get all
 async def get_polls(request):
+    """Get all polls."""
     async with create_connection(request.app) as conn:
         conn.row_factory = Row
         cur = await conn.execute("SELECT * FROM question;")
         data = await cur.fetchall()
-        return web.json_response([dict(row) for row in data])
+    return web.json_response([dict(row) for row in data])
 
-# post new
 async def post_polls(request):
+    """Create a new poll.
+
+    The endpoint takes a JSON object in the following format:
+    {
+      "question_text": "What question would you like to ask?",
+      "choices": [
+        "One choice",
+        "Another choice"
+      ]
+    }
+    """
     try:
         data = await request.json()
         question_text = str(data["question_text"])
@@ -51,11 +58,11 @@ async def post_polls(request):
             [(choice_text, question_id) for choice_text in choices],
         )
         await conn.commit()
-        return web.json_response({"question_id": question_id})
+    return web.json_response({"question_id": question_id})
 
-# get one
-# /polls/{id}
+
 async def get_poll(request):
+    """Get info about a particular poll."""
     poll_id = int(request.match_info["poll_id"])
     async with create_connection(request.app) as conn:
         conn.row_factory = Row
@@ -71,16 +78,49 @@ async def get_poll(request):
         res = await cur.fetchall()
         poll["choices"] = [dict(choice) for choice in res]
 
-        return web.json_response(poll)
-# put?
+    return web.json_response(poll)
 
-# Choice
-# /polls/{id}/choices
-# post new choice
+
+# CHOICES
+
 async def post_poll_choices(request):
     return web.json_response({"resource": "post_poll_choices"})
 
-# /polls/{id}/choices/{id}
-# put to vote
+
 async def put_poll_choice(request):
+    """Vote on a poll choice.
+
+    The endpoint takes a JSON object in the following format:
+    {
+      "vote": true
+    }
+    """
+    poll_id = int(request.match_info["poll_id"])
+    choice_id = int(request.match_info["choice_id"])
+    try:
+        data = await request.json()
+        vote = bool(data["vote"])
+    except (KeyError, TypeError, json.decoder.JSONDecodeError):
+        raise web.HTTPBadRequest()
+
+    if vote:
+        async with create_connection(request.app) as conn:
+            conn.row_factory = Row
+            await conn.execute(
+                "UPDATE choice SET votes = votes + 1 WHERE question_id=? AND id=?;",
+                (poll_id, choice_id),
+            )
+            await conn.commit()
+
+            cur = await conn.execute(
+                "SELECT * FROM choice WHERE question_id=? AND id=?;",
+                (poll_id, choice_id),
+            )
+            res = await cur.fetchone()
+            if res is None:
+                raise web.HTTPNotFound()
+
+        choice = dict(res)
+        return web.json_response(choice)
+
     return web.json_response({"resource": "put_poll_choice"})
